e6209c618f7e4a7710a4ab78e8a7d211e77f8c6d
KerstenSchaller <29780537*KerstenSchaller@users.noreply.github.com>
2018-09-16 12:32:56 +0200

    add

---
+++ ImageOrganizer/ImageOrganizer.cs
@@ -0,0 +1,88 @@
+﻿using System;
+using System.Collections.Generic;
+using System.IO;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace ImageOrganizer
+{
+    class ImageOrganizer
+    {
+        /*Variable definitions*/
+        ImageOrganizerFileInfo iOFileInfo = new ImageOrganizerFileInfo();
+        string path;
+        
+
+        /*Constructor*/
+        public ImageOrganizer(string _path)
+        {
+            path = _path;
+            DirectoryInfo di = new DirectoryInfo(path);
+            iOFileInfo.fileInfos = di.GetFiles();
+            iOFileInfo.labels = new string[iOFileInfo.fileInfos.Length];
+
+            /*Initialize Labels*/
+            for (int i = 0; i < iOFileInfo.labels.Count(); i++)
+            {
+                iOFileInfo.labels[i] = "";
+            }
+
+            /*Add standart labels*/
+            addLabel("_loeschen");
+            addLabel("_bearbeiten");
+            addLabel("_weiterSortieren");
+
+        }
+
+        public void addLabel(string newlabel)
+        {
+            iOFileInfo.labeltypes.Add(newlabel);
+        }
+
+        /*Set Label for certain image*/
+        public void setLabel(int Index, string label)
+        {
+            iOFileInfo.labels[Index] = label;
+        }
+
+        public void ApplyChanges()
+        {
+            FileInfo[] fi = iOFileInfo.fileInfos;
+            string[] labels = iOFileInfo.labels;
+
+
+            foreach (string label in iOFileInfo.labeltypes)
+            {
+                string dirlabel = path + @"\" + label;
+                Directory.CreateDirectory(dirlabel);
+            }
+            
+
+            for (int i = 0; i < fi.Length; i++)
+            {
+                if (labels[i] != "")
+                {
+                    string src = fi[i].FullName;
+                    string dest = path + @"\" + labels[i] + @"\" + Path.GetFileName(src);
+                    File.Move(src,dest);
+
+                }
+            }
+        }
+
+
+    }
+
+    class ImageOrganizerFileInfo
+    {
+        public FileInfo[] fileInfos;
+        public int ImageIndex;
+        public string[] labels;
+        public List<string> labeltypes = new List<string>();
+
+        public ImageOrganizerFileInfo() { }
+
+
+    }
+}
